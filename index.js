b9386fe34a6a8614e51d7b8c3369fbdc41f1dc5e
Yasmin Valim <yasmin.valimds@gmail.com>
2023-05-03 20:00:53 +0100

    Adding close button

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const menuIcon = document.querySelector(".menu-icon");
 const navLinks = document.querySelector("nav ul");
 const links = document.querySelectorAll("nav ul li");
 const fullscreenMenu = document.querySelector(".fullscreen-menu");
+const closeMenuIcon = document.querySelector(".menu-close-button")
 
 menuIcon.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
+  navLinks.classList.add("show");
 
   links.forEach((link) => {
-    link.classList.toggle("fade");
+    link.classList.add("fade");
   });
 
-  fullscreenMenu.classList.toggle("show");
+  fullscreenMenu.classList.add("show");
+});
+
+closeMenuIcon.addEventListener("click", () => {
+  navLinks.classList.remove("show");
+
+  links.forEach((link) => {
+    link.classList.remove("fade");
+  });
+
+  fullscreenMenu.classList.remove("show");
 });
