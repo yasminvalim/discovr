77703c95082493cff4e0bfc8258d550cd68b4270
Yasmin Valim <yasmin.valimds@gmail.com>
2023-05-06 00:06:37 +0100

    create a toggle to favorite

--- favorite.js
+++ favorite.js
@@ -1,10 +1,3 @@
-const stars = document.querySelectorAll('.star');
-
-stars.forEach(star => {
-  star.addEventListener('click', function() {
-    const value = this.getAttribute('data-value');
-    for (let i = 0; i < value; i++) {
-      stars[i].classList.toggle('favorite');
-    }
-  });
-});
+function myFavorite(x) {
+   x.classList.toggle("fa-solid");
+}
