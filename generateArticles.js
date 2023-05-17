34be82c57579f6dbfacf32083c27329eb99091ee
Yasmin Valim <yasmin.valimds@gmail.com>
2023-05-17 20:14:27 +0100

    some changes

--- generateArticles.js
+++ generateArticles.js
@@ -3,14 +3,19 @@ const myArticleContainer = document.querySelector('#myArticleContainer');
 const gridItem = document.createElement('div');
 gridItem.classList.add('grid-item');
 gridItem.innerHTML = `
-  <img
-    src="https://s3.amazonaws.com/vinils3/wp-content/uploads/2014/06/Neworder_technique_capa.jpg"
-    alt="album cover technique"
-    class="album-cover"
-  />
-  <div class="star">
-    <i onclick="myFavorite(this)" class="fa-regular fa-star"></i>
-  </div>
+<div class="grid-item">
+        <img
+        src="https://s3.amazonaws.com/vinils3/wp-content/uploads/2014/06/Neworder_technique_capa.jpg"
+        alt="album cover technique"
+        class="album-cover"
+      />
+      <p class="album-title"> Tecnique - New Order </p>
+      <div class="star">
+        <i onclick="myFavorite(this)" class="fa-regular fa-star"></i>
+      </div>
+      </div>
+      <div class="grid-item">
+      </div>
 `;
 
 myArticleContainer.appendChild(gridItem);
